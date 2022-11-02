63f19c75d789d55b2cad8b3e006d59d46abe116e
Dennis Au <e46mpwr@users.noreply.github.com>
2022-11-02 13:34:45 -0700

    Update apiKey.js

--- apiKey.js
+++ apiKey.js
@@ -1,6 +1,6 @@
 /* Copy this into a file named apiKey.js */
 const apiKey = '1DMKLHnhNJuks6L7FGogNe0OpsC1mMilAdhngJLsk';
 const appId = 'io.croquet.web_showcase_jja';
-const name = "jja_web_showcase";
+const name = "jja_web_showcase_v2";
 const password = "jja_web_showcase";
 export default {apiKey, appId, name, password};
